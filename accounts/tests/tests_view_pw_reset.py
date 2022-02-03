d41c53798d082ae65aac089b6f75d7e0b28998be
edizbak <valla_jo@hotmail.com>
2022-02-03 13:34:54 +0100

    fin reset password

--- accounts/tests/tests_view_pw_reset.py
+++ accounts/tests/tests_view_pw_reset.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.models import User
 from django.core import mail
 from django.urls import reverse, resolve
@@ -65,3 +69,92 @@ class InvalidPwResetTests(TestCase):
 
 	def test_no_reset_email_sent(self):
 		self.assertEqual(0, len(mail.outbox))
+
+
+class PasswordResetDoneTests(TestCase):
+	def setUp(self):
+		url = reverse('password_reset_done')
+		self.response = self.client.get(url)
+
+	def test_status_code(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_view_function(self):
+		view = resolve('/reset/done/')
+		self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
+
+
+class PasswordResetConfirmTests(TestCase):
+	def setUp(self):
+		email = 'jeanjean@coin.fr'
+		user = User.objects.create_user(username='Jean', email=email, password='1234abcdef')
+		
+		'''
+		on créé un token de réinitialisation valide en se basant sur la méthode utilisée par Django :
+
+		'''
+		self.uid = urlsafe_base64_encode(force_bytes(user.pk))
+		self.token = default_token_generator.make_token(user)
+
+		url = reverse('password_reset_confirm', kwargs={'uidb64': self.uid, 'token': self.token})
+		self.response = self.client.get(url, follow=True)
+
+	def test_status_code(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_view_function(self):
+		view = resolve('/reset/{uidb64}/{token}/'.format(uidb64=self.uid, token=self.token))
+		self.assertEquals(view.func.view_class, auth_views.PasswordResetConfirmView)
+		
+	def test_csrf(self):
+		self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+	def test_contains_form(self):
+		form = self.response.context.get('form')
+		self.assertIsInstance(form, SetPasswordForm)
+
+	def test_form_inputs(self):
+		'''
+		La vue doit contenir deux entrées : csrf et deux champs passsword
+		'''
+		self.assertContains(self.response, '<input', 3)
+		self.assertContains(self.response, 'type="password"', 2)
+
+
+class InvalidPwResetConfirmTests(TestCase):
+	def setUp(self):
+		email = 'jeanjean@coin.fr'
+		user = User.objects.create_user(username='Jean', email=email, password='1234abcdef')
+		uid = urlsafe_base64_encode(force_bytes(user.pk))
+		token = default_token_generator.make_token(user)	
+
+		
+		'''
+		on invalide le token en changeant le mot de passe
+		'''	
+		user.set_password('abcdef1234')
+		user.save()
+
+		url = reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})
+		self.response = self.client.post(url, {'email': 'jeanjean@coin.fr'})
+
+	def test_status_code(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_html(self):
+		pw_reset_url = reverse('password_reset')
+		self.assertContains(self.response, 'lien de réinitialisation invalide')
+		self.assertContains(self.response, 'href="{0}"'.format(pw_reset_url))
+
+
+class PasswordResetCompleteTests(TestCase):
+	def setUp(self):
+		url = reverse('password_reset_complete')
+		self.response = self.client.get(url)
+
+	def test_status_code(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_view_function(self):
+		view = resolve('/reset/complete/')
+		self.assertEquals(view.func.view_class, auth_views.PasswordResetCompleteView)
